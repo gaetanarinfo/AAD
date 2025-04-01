1a39ec59805bbd49d36b60222ae8d436e4b61d3a
Seigneur Gaëtan <seignzurgaetan03@gmail.com>
2025-04-01 13:25:51 +0200

    Modifications 31/03/2025

--- src/pages/auth/AddPrestationPage.vue
+++ src/pages/auth/AddPrestationPage.vue
@@ -180,15 +180,34 @@
 
           <q-form @submit="showCoordonnees()">
 
-            <q-input dense filled v-model="event" :model-value="`${event.from} - ${event.to}`"
-              label="Date de prestation *" lazy-rules
-              :rules="[val => val && val.length > 0 || 'S\'il vous plaît tapez quelque chose !']">
+            <q-input dense filled v-model="event" mask="##/##/####" label="Début de prestation *" :rules="[checkDate]">
               <template v-slot:prepend>
 
                 <q-icon name="event" class="cursor-pointer">
 
                   <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date range v-model="event">
+                    <q-date @input="() => $refs.qDateProxy.hide()" mask="DD/MM/YYYY" :options="optionsCalendar"
+                      v-model="event">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Fermer" color="light-blue-9" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+
+                </q-icon>
+
+              </template>
+
+            </q-input>
+
+            <q-input dense filled v-model="event2" mask="##/##/####" label="Fin de prestation *" :rules="[checkDate]">
+              <template v-slot:prepend>
+
+                <q-icon name="event" class="cursor-pointer">
+
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date @input="() => $refs.qDateProxy.hide()" :options="optionsCalendar2" mask="DD/MM/YYYY"
+                      v-model="event2">
                       <div class="row items-center justify-end">
                         <q-btn v-close-popup label="Fermer" color="light-blue-9" flat />
                       </div>
@@ -388,9 +407,65 @@
 
       <div class="w-100 flex flex-center column" style="margin-bottom: 50px;">
 
-        <h5 class="text-center" style="margin-top: 1rem;margin-bottom: 1rem;">Lieu de la prestation</h5>
+        <h5 class="text-center" style="margin-top: 1rem;margin-bottom: 1rem;">Paiement de la prestation</h5>
 
         <div class="w-100 text-center q-pl-md q-pr-md form-w">
+
+          <q-list bordered separator>
+
+            <q-item v-for="item_price in price_ht_hour">
+
+              <q-item-section style="width: 25px; height: 25px; max-width: 25px;">
+                <q-img style="width: 30px; height: 30px;" :src="folderAPI + '/services/' + item_price.icone" />
+              </q-item-section>
+
+              <q-item-section class="text-left" style="margin-left: 16px;margin-right: 16px;">
+                <q-item-label caption lines="1"><strong>{{ item_price.name }}</strong></q-item-label>
+                <q-item-label caption lines="2"
+                  style="margin-top: 0.5rem;overflow:initial;text-wrap: initial;text-transform: none;display: block !important;">Prestation
+                  de service <span class="text-lowercase">{{
+                    item_price.name
+                  }} du {{ event }} à {{ replace(':', 'h', event_hour_start) }} au {{ event2 }} à {{ replace(':', 'h',
+                      event_hour_end)
+                    }}</span></q-item-label>
+                <q-item-label caption lines="4"><strong>Prix de l'heure :</strong> {{ item_price.price_ttc_hour }}
+                  €</q-item-label>
+              </q-item-section>
+
+            </q-item>
+
+          </q-list>
+
+          <q-card class="text-white glossy bg-light-blue-9" bordered style="margin-top: 30px;">
+
+            <q-item>
+
+              <q-item-section class="text-right row content-end">
+                <q-item-label><strong>Crédit d'impôt :</strong> {{
+                  credit_impot }} %</q-item-label>
+                <q-item-label><strong>Total des jours de pretation :</strong> {{ cart_day }} jour(s)</q-item-label>
+                <q-item-label><strong>Total des heures de pretation :</strong> {{ cart_hour }} heure(s)</q-item-label>
+                <q-item-label><strong>Total :</strong> {{ cart_ttc }} €</q-item-label>
+              </q-item-section>
+
+            </q-item>
+
+          </q-card>
+
+          <q-item style="margin-bottom: 30px;margin-top:30px;padding-top: 0;justify-content: center;" class="w-100">
+            <q-img src="~assets/paiement-securise-cb.jpeg" style="max-width: 230px;" spinner-color="light-blue-9" />
+          </q-item>
+
+          <div class="row-center w-100" style="margin-top: 30px;margin-bottom: 30px;">
+
+            <q-btn @click="setPaiementPrestation" glossy v-ripple padding="10px" class="w-100" dense
+              color="light-blue-9">
+
+              <div style="font-weight: 600;">Payer maintentant</div>
+
+            </q-btn>
+
+          </div>
 
         </div>
 
@@ -438,7 +513,8 @@ const connexionState = ref(true),
   services = ref([]),
   serviceSelected = ref([]),
   serviceActive = ref(false),
-  event = ref({ from: '', to: '' }),
+  event = ref(''),
+  event2 = ref(''),
   event_hour_start = ref(''),
   event_hour_end = ref(''),
   adresses = ref([]),
@@ -449,7 +525,16 @@ const connexionState = ref(true),
   showAdresses = ref(false),
   loaderAdresse = ref(false),
   options = ref([]),
-  loaderGeolocation = ref(false)
+  loaderGeolocation = ref(false),
+  price_ht_hour = ref([]),
+  credit_impot_active = ref(false)
+
+
+// Cart
+const cart_ttc = ref(0.00),
+  credit_impot = ref(50),
+  cart_day = ref(0),
+  cart_hour = ref(0)
 
 export default defineComponent({
   name: 'InboxtPage',
@@ -524,7 +609,77 @@ export default defineComponent({
     }
 
     return {
+      credit_impot_active,
+      cart_ttc,
+      cart_day,
+      cart_hour,
+      credit_impot,
+      price_ht_hour,
       loaderGeolocation,
+      setPaiementPrestation () {
+
+        prestation_show.value = false
+        service_show.value = false
+        planning_show.value = false
+        coordonnees_show.value = false
+        paiement_show.value = false
+        logo.value = false
+
+        setTimeout(() => {
+          loader.value = true
+        }, 1200);
+
+        axios.post(process.env.API + '/api/paypal/order-insert-services',
+          {
+            start_prestation: event.value.split('/').reverse().join('-'),
+            end_prestation: event2.value.split('/').reverse().join('-'),
+            hour_start_prestation: event_hour_start.value.replace('h', ':'),
+            hour_end_prestation: event_hour_end.value.replace('h', ':'),
+            total_days: cart_day.value,
+            total_hours: cart_hour.value,
+            total_ttc: cart_ttc.value,
+            services_id: serviceSelected.value,
+            companie_id: companieSelected.value,
+            user: {
+              id: userStore.stateUser.user.id,
+              adresse_prestation: adresse.value,
+              ville_prestation: ville.value,
+              code_postal_prestation: codepostal.value,
+              pays_prestation: pays.value.value
+            }
+          }
+        ).then(res => {
+
+          if (res.data.succes) {
+
+            location.href = process.env.API + '/api/paypal/create-order-services/' + res.data.order_id
+
+          } else {
+
+            $q.notify({
+              timeout: 2000,
+              color: 'red-5',
+              textColor: 'white',
+
+              icon: 'warning',
+              message: res.data.message,
+              progress: true,
+              classes: 'glossy',
+            })
+
+          }
+
+        })
+
+        // setTimeout(() => {
+        //   loader.value = false
+        // }, 3500);
+
+        // setTimeout(() => {
+        //   logo.value = true
+        // }, 4000);
+
+      },
       searchGeolocation () {
 
         if (connexionState) {
@@ -588,6 +743,44 @@ export default defineComponent({
         coordonnees_show.value = false
         paiement_show.value = false
         logo.value = false
+
+        axios.get(process.env.API + '/api/user/user-choices-tarifs/' + companieSelected.value + '/' + serviceSelected.value)
+          .then(res => {
+
+            if (res.data.succes) {
+
+              if (res.data.price.length >= 1) {
+
+                let totalCount = 0;
+
+                price_ht_hour.value = res.data.price
+
+                // Extraction des datas pour le panier
+                res.data.price.forEach(element => {
+                  let price = parseFloat(element.price_ttc_hour)
+                  totalCount += price
+                  credit_impot_active.value = (element.credit_impot >= 1) ? true : false
+                });
+
+                // Calcul du nombre de jour de prestation et des heures
+
+                let date1 = event.value.split('/').reverse().join('-');
+                let date2 = event2.value.split('/').reverse().join('-');
+                let heure1 = moment(moment().format('YYYY-MM-DD') + ' ' + event_hour_start.value.replace('h', ':'));
+                let heure2 = moment(moment().format('YYYY-MM-DD') + ' ' + event_hour_end.value.replace('h', ':'));
+
+                cart_day.value = (moment(date2).diff(moment(date1), "days") === 0) ? 1 : moment(date2).diff(moment(date1), "days")
+                cart_hour.value = heure2.diff(heure1, "hours")
+
+                cart_ttc.value = (credit_impot_active.value) ? totalCount * parseInt(cart_hour.value) * parseInt(cart_day.value) / 2 : totalCount * parseInt(cart_hour.value) * parseInt(cart_day.value)
+
+              } else {
+                price_ht_hour.value = []
+              }
+
+            }
+
+          })
 
         setTimeout(() => {
           loader.value = true
@@ -696,7 +889,11 @@ export default defineComponent({
       event_hour_start,
       event_hour_end,
       event,
-      optionsFn (date) {
+      event2,
+      optionsCalendar (date) {
+        return date >= moment().format('YYYY/MM/DD')
+      },
+      optionsCalendar2 (date) {
         return date >= moment().format('YYYY/MM/DD')
       },
       showCalendar () {
@@ -987,6 +1184,13 @@ export default defineComponent({
 
       // this is actually what the default style-fn does in Quasar
       return { minHeight: offset ? `auto` : 'auto' }
+    },
+    checkDate (val) {
+      if (!val) return 'Date invalide.'
+    },
+    replace: function (st, rep, repWith) {
+      const result = repWith.replace(st, rep)
+      return result;
     }
   },
   watch: {
